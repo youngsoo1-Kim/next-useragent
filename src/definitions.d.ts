4a4615be16ee4713300c70410e2f11b0366b3ed9
tokuda109 <tokuda109@gmail.com>
2019-04-10 10:25:13 +0900

    feat: separated definition file & bump version

---
+++ src/definitions.d.ts
@@ -0,0 +1,14 @@
+
+export interface UserAgentType {
+  source: string,
+  device: string,
+  deviceVersion: string,
+  os: string,
+  osVersion: string,
+  browser: string,
+  browserVersion: string,
+  isMobile: boolean,
+  isTablet: boolean,
+  isDesktop: boolean,
+  isBot: boolean
+}
--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,7 @@
 import { NextContext } from 'next'
 import * as useragent from 'useragent'
 
-export interface UserAgentType {
-  source: string,
-  device: string,
-  deviceVersion: string,
-  os: string,
-  osVersion: string,
-  browser: string,
-  browserVersion: string,
-  isMobile: boolean,
-  isTablet: boolean,
-  isDesktop: boolean,
-  isBot: boolean
-}
+import { UserAgentType } from './definitions'
 
 class UserAgent implements UserAgentType {
 
